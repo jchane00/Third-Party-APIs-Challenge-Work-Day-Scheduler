102b5ca94c1eb5ccbb843c913bb9bb7ca245a21a
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-08-12 12:21:22 -0500

    updated Readme

--- challenge_5/Develop/script.js
+++ challenge_5/Develop/script.js
@@ -1,9 +1,23 @@
-// localStorage.clear()
-// Local Storage
-let planner_data = (localStorage.getItem("planner_data") !== null) ? JSON.parse(localStorage.getItem("planner_data")) : {"9": "", "10": "", "11": "", "12": "", "1": "", "2": "", "3": "", "4": "", "5": ""}
-var saveSchedule = function(planner_data) {
-    localStorage.setItem("planner_data", JSON.stringify(planner_data))
-}
+
+// Local Storage (debug)
+
+// let planner_data = (localStorage.getItem("planner_data") !== null) ? JSON.parse(localStorage.getItem("planner_data")) : {"9": "", "10": "", "11": "", "12": "", "1": "", "2": "", "3": "", "4": "", "5": ""}
+// var saveSchedule = function(planner_data) {
+//     localStorage.setItem("planner_data", JSON.stringify(planner_data))
+// }
+
+// let textarea = $(this).children("textarea")
+//   let button = $(this).children("button")
+
+//   let id = button.parent().prop("id")
+//   textarea.val(planner_data[id]) 
+//   button.click(function(){
+    
+//     planner_data[id] = textarea.val()
+//     console.log(planner_data)
+//     saveSchedule(planner_data)
+//   })
+
 // Time and Date variables
 var today = new Date();
 var hour = today.getHours() % 12 || 12;
@@ -16,7 +30,6 @@ $(function() {
 })
 
 
-console.log(planner_data)
 var addColor = $(".timeblock").each(function(elem) {
  if ((parseInt($(this).prop("id"))  < (hour))){
   $(this).addClass("btn btn-secondary")
@@ -32,19 +45,7 @@ var addColor = $(".timeblock").each(function(elem) {
 
     
   
-  let textarea = $(this).children("textarea")
-  let button = $(this).children("button")
-
-  let id = button.parent().prop("id")
-  textarea.val(planner_data[id]) 
-  button.click(function(){
-    
-    
-
-    planner_data[id] = textarea.val()
-    console.log(planner_data)
-    saveSchedule(planner_data)
-  })
+ 
  
   
   
